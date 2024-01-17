b25d2bc007df01477b892a811591e81f96519f91
isaac-svg <isaacsakyi009@gmail.com>
2024-01-17 14:29:54 -0500

    all

--- models/review.py
+++ models/review.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This is the review class"""
+""" Review class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, ForeignKey, Float
